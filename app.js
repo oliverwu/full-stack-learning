150083d8561eeb296fc6e1ddc694a3e31ffa732f
Oliver <oliver@Olivers-MacBook-Pro-16-inch.local>
2020-09-03 14:52:04 +1000

    update

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ const express = require('express')
 const app = express()
 const port = 3001
 
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
 app.get('/', (req, res) => {
-console.log('Hello World!')	
-   res.send('Hello World! EC2-HK')
+	console.log('Hello World!')	
+   	res.send('Hello World! EC2-HK')
+})
+
+app.post('/test', (req, res) => {
+	res.set('x-toke', 'dhfjksfjkasdl;fjd;salk')
+	res.json(req.body)
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  	console.log(`Example app listening at http://localhost:${port}`)
 })
